59b0ade0529fe421b825b8da69df7ff6cdd03007
areicherdt <andre@gmx.at>
2015-01-26 17:20:56 +0100

    hopefully fix piwik import

---
+++ js/piwik.js
@@ -0,0 +1,14 @@
+/*!
+ * JavaScript needed by Piwik to create report
+ */
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+
+(function() {
+	var u="//stats.anonimecs.com/";
+	_paq.push(['setTrackerUrl', u+'piwik.php']);
+	_paq.push(['setSiteId', 1]);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
